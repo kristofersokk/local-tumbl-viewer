ab39d6597da6a44abec8648eef56998ee13b3d6f
Kristofer Sokk <krsokk@taltech.ee>
2025-08-27 23:05:38 +0300

    Install posthog

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { PostHogProvider } from 'posthog-js/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
-import { RouterProvider, createRouter } from '@tanstack/react-router';
-
 import { routeTree } from './routeTree.gen';
 
 const router = createRouter({ routeTree });
@@ -15,6 +14,16 @@ declare module '@tanstack/react-router' {
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<PostHogProvider
+			apiKey="phc_VtDN9SZkBb6IZVHA86b1lRTSMWmBy67PecUm7Y3mNcS"
+			options={{
+				api_host: 'https://eu.i.posthog.com',
+				defaults: '2025-05-24',
+				capture_exceptions: true,
+				debug: import.meta.env.DEV,
+			}}
+		>
+			<RouterProvider router={router} />
+		</PostHogProvider>
 	</StrictMode>
 );
