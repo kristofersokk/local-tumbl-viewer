7f25c726ea92dc87816b12fa209fd0ac621076cc
Kristofer Sokk <krsokk@taltech.ee>
2025-09-09 23:28:27 +0300

    Remove posthog, only rely on vercel statistics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { RouterProvider, createRouter } from '@tanstack/react-router';
-import { PostHogProvider } from 'posthog-js/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { routeTree } from './routeTree.gen';
@@ -14,16 +13,6 @@ declare module '@tanstack/react-router' {
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<PostHogProvider
-			apiKey="phc_VtDN9SZkBb6IZVHA86b1lRTSMWmBy67PecUm7Y3mNcS"
-			options={{
-				api_host: 'https://eu.i.posthog.com',
-				defaults: '2025-05-24',
-				capture_exceptions: true,
-				debug: import.meta.env.DEV,
-			}}
-		>
-			<RouterProvider router={router} />
-		</PostHogProvider>
+		<RouterProvider router={router} />
 	</StrictMode>
 );
